a2544ddb1aa9a1e7812490ee4fb75b2f4d0b456d
elad rittal <eladrittal@gmail.com>
2020-01-22 11:00:09 +0200

    first commit

---
+++ CLI/src/pages/HOME.c1dmp/pageElements.d.ts
@@ -0,0 +1,10 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#repeater1": $w.Repeater;
+  "#container1": $w.Container;
+  "#text1": $w.Text;
+  "#text2": $w.Text;
+  "#iconButton1": $w.Button;
+  "#header1": $w.Header;
+  "#footer1": $w.Footer;
+}
